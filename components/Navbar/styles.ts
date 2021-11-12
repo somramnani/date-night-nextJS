8d63de12ca4140dc1ee935e99df8bc782ffa1b3d
somramnani <som.ramnani@gmail.com>
2021-11-12 16:09:48 -0500

    a basic navbar

--- components/Navbar/styles.ts
+++ components/Navbar/styles.ts
@@ -1,15 +1,7 @@
 import { makeStyles } from '../../lib/utils/makeStyles'
 
 const useStyles = makeStyles()((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
+  
 }))
 
 export default useStyles
