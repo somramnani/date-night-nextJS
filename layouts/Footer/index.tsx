fb64655b23bc93326f1866952bb59144f44e5e72
somramnani <som.ramnani@gmail.com>
2021-11-12 13:09:00 -0500

    edit Footer layout

--- layouts/Footer/index.tsx
+++ layouts/Footer/index.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react'
 import useStyles from './styles'
+import { Container } from '@mui/material'
+import { Grid } from '@mui/material'
+import { Box } from '@mui/material'
+import Link from 'next/link'
 
 const Footer: FC = () => {
   const { classes } = useStyles()
-  return <div>sdfds</div>
+  return <footer></footer>
 }
 export default Footer
